49911e3ab408a87801f94716536a83923688dbf9
Christopher Elwell <elwell.christopher@gmail.com>
2019-01-28 22:04:58 -0800

    Initial Commit

---
+++ Sunscreen/SunscreenView.h
@@ -0,0 +1,13 @@
+//
+//  SunscreenView.h
+//  Sunscreen
+//
+//  Created by Christopher Elwell on 1/28/19.
+//  Copyright © 2019 Christopher Elwell. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SunscreenView : ScreenSaverView
+
+@end
---
+++ Sunscreen/SunscreenView.m
@@ -0,0 +1,52 @@
+//
+//  SunscreenView.m
+//  Sunscreen
+//
+//  Created by Christopher Elwell on 1/28/19.
+//  Copyright © 2019 Christopher Elwell. All rights reserved.
+//
+
+#import "SunscreenView.h"
+
+@implementation SunscreenView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
