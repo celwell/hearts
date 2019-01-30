11e182c4d4fbcfd968436a03a36f30b326528ddc
Christopher Elwell <elwell.christopher@gmail.com>
2019-01-29 16:25:45 -0800

    refactor & add comments

--- Hearts/HeartsView.m
+++ Hearts/HeartsView.m
@@ -36,88 +36,90 @@
 
 - (void)animateOneFrame
 {
-    NSBezierPath *path, *path2, *path3;
+    NSBezierPath *topLeftArcPath, *topRightArcPath, *rectPath;
     NSRect rect;
     NSSize screenSize;
-    NSColor *color;
-    float alpha;
-
-    screenSize = [self bounds].size;
-    
-    float edgeLength = SSRandomFloatBetween(screenSize.width / 100.0,
-                                            screenSize.width / 10.0);
-    
-    rect.size = NSMakeSize(edgeLength, edgeLength);
-    // Calculate random origin point
-    rect.origin = SSRandomPointForSizeWithinRect( rect.size, [self bounds] );
-    
-    float startAngle, endAngle, radius;
     NSPoint point;
+    // color values (0 - 1)
+    float red, green, blue, alpha;
+    float edgeLength, radius, sqLen;
     
-    path = [NSBezierPath bezierPath];
-    startAngle = 45.0;
-    endAngle = 225.0;
+    screenSize = [self bounds].size;
+    edgeLength = SSRandomFloatBetween(screenSize.width / 100.0,
+                                            screenSize.width / 10.0);
+    rect.size = NSMakeSize(edgeLength, edgeLength);
+    // calculate random origin point
+    rect.origin = SSRandomPointForSizeWithinRect(rect.size, [self bounds]);
+    
+    // arc dimensions common to both heart arcs
+    sqLen = 1.4142135 * (rect.size.width / 4);
     radius = rect.size.width / 2;
-    // Calculate our center point
     
-    //a = radius /2
-    float sqLen = 1.4142135 * (rect.size.width / 4);
-    
+    // top-left heart arc
+    topLeftArcPath = [NSBezierPath bezierPath];
+    // center point of top-left heart arc
     point = NSMakePoint(rect.origin.x - sqLen,
                         rect.origin.y + sqLen );
-    [path appendBezierPathWithArcWithCenter: point
+    [topLeftArcPath appendBezierPathWithArcWithCenter: point
                                      radius: radius
-                                 startAngle: startAngle
-                                   endAngle: endAngle
+                                 startAngle: 45.0
+                                   endAngle: 225.0
                                   clockwise: 0];
     
-    path2 = [NSBezierPath bezierPath];
-    startAngle = 135.0;
-    endAngle = 315.0;
-    radius = rect.size.width / 2;
-    // Calculate our center point
+    // top-right heart arc
+    topRightArcPath = [NSBezierPath bezierPath];
+    // center point of top-right heart arc
     point = NSMakePoint(rect.origin.x + sqLen,
                         rect.origin.y + sqLen );
-    [path2 appendBezierPathWithArcWithCenter: point
+    [topRightArcPath appendBezierPathWithArcWithCenter: point
                                      radius: radius
-                                 startAngle: startAngle
-                                   endAngle: endAngle
+                                 startAngle: 135.0
+                                   endAngle: 315.0
                                   clockwise: 1];
     
-    path3 = [NSBezierPath bezierPathWithRect:rect];
+    // rotated square to create body of heart
+    rectPath = [NSBezierPath bezierPathWithRect:rect];
     NSAffineTransform * transform = [NSAffineTransform transform];
     [transform translateXBy: rect.origin.x yBy: rect.origin.y];
     [transform rotateByDegrees:45.0];
     [transform translateXBy: (-1 * rect.origin.x) yBy: (-1 * rect.origin.y)];
-    path3 = [transform transformBezierPath:path3];
+    rectPath = [transform transformBezierPath:rectPath];
+    // I assume there is some way to add this to the prior transform, anyways...
     transform = [NSAffineTransform transform];
-    [transform translateXBy: 0 yBy: (-1.414 * radius)];
-    path3 = [transform transformBezierPath:path3];
+    // not quite -1.414 because we want the anti-aliasing to overlap slightly
+    // this could be improved...
+    [transform translateXBy: 0 yBy: (-1.411 * radius)];
+    rectPath = [transform transformBezierPath:rectPath];
     
+    // 50:50 chance of pink or black heart
+    if (SSRandomIntBetween( 0, 1 ) == 0) {
+        // 50:50 chance of light pink or medium pink heart
+        if (SSRandomIntBetween( 0, 1 ) == 0) {
+            // light pink
+            red = 255.0 / 255.0;
+            green = 159.0 / 255.0;
+            blue = 243.0 / 255.0;
+        } else {
+            // medium pink
+            red = 243.0 / 255.0;
+            green = 104.0 / 255.0;
+            blue = 224.0 / 255.0;
+        }
+    } else {
+        // black
+        red = green = blue = 0;
+    }
     
+    // hearts have randomized transparency
     alpha = SSRandomFloatBetween( 160.0, 255.0 ) / 255.0;
     
-    if (SSRandomIntBetween( 0, 1 ) == 0)
-        if (SSRandomIntBetween( 0, 1 ) == 0)
-            color = [NSColor colorWithCalibratedRed:(255.0 / 255.0)
-                                              green:(159.0 / 255.0)
-                                               blue:(243.0 / 255.0)
-                                              alpha:alpha];
-        else
-            color = [NSColor colorWithCalibratedRed:(243.0 / 255.0)
-                                          green:(104.0 / 255.0)
-                                           blue:(224.0 / 255.0)
-                                          alpha:alpha];
-    else
-        color = [NSColor colorWithCalibratedRed:0
-                                          green:0
-                                           blue:0
-                                          alpha:alpha];
+    // set color of the heart
+    [[NSColor colorWithCalibratedRed:red green:green blue:blue alpha:alpha] set];
     
-    [color set];
-    [path fill];
-    [path2 fill];
-    [path3 fill];
+    // fill all the paths for this heart
+    [topLeftArcPath fill];
+    [topRightArcPath fill];
+    [rectPath fill];
     
     return;
 }
